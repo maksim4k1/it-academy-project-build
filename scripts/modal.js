d2f2d4234dbcdb43df97e0ce3e12c3b66b5b3b99
maksim4k1 <maksim4k1@gmail.com>
2023-11-29 00:26:35 +0700

    edited functionality for open/close modal and added close modal handler to modal bg

--- scripts/modal.js
+++ scripts/modal.js
@@ -5,6 +5,17 @@ const openModalButtonEl = document.getElementById("open-modal-button");
 
 let modalIsOpen = false;
 
+function closeModal(){
+  document.body.style.overflow = "auto";
+  modalEl.classList.remove("modal--opened");
+  modalIsOpen = false;
+}
+function openModal(){
+  document.body.style.overflow = "hidden";
+  modalEl.classList.add("modal--opened");
+  modalIsOpen = true;
+}
+
 function closeButtonHandler(){
   if(modalIsOpen){
     document.body.style.overflow = "auto";
@@ -17,6 +28,9 @@ function closeButtonHandler(){
   modalIsOpen = !modalIsOpen;
 }
 
-closeModalFormButtonEl.onclick = closeButtonHandler;
-closeModalButtonEl.onclick = closeButtonHandler;
-openModalButtonEl.onclick = closeButtonHandler;
+modalEl.onclick = (event) => {
+  if(event.target === event.currentTarget) closeModal();
+}
+closeModalFormButtonEl.onclick = closeModal;
+closeModalButtonEl.onclick = closeModal;
+openModalButtonEl.onclick = openModal;
