405c2365dc6e6eec6ed489cd34a9c4f376636995
maksim4k1 <maksim4k1@gmail.com>
2023-12-24 13:46:41 +0700

    fixed document scroll, when modal window is open

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,30 +2,19 @@ const modalEl = document.getElementById("modal");
 const closeModalFormButtonEl = document.getElementById("close-modal-form-button");
 const closeModalButtonEl = document.getElementById("close-modal-button");
 const openModalButtonEl = document.getElementById("open-modal-button");
+const htmlEl = document.getElementById("html");
 
 let modalIsOpen = false;
 
 function closeModal(){
-  document.body.style.overflow = "auto";
+  htmlEl.style.overflow = "auto";
   modalEl.classList.remove("modal--visible");
   modalIsOpen = false;
 }
 function openModal(){
-  document.body.style.overflow = "hidden";
+  htmlEl.style.overflow = "hidden";
   modalEl.classList.add("modal--visible");
   modalIsOpen = true;
-}
-
-function closeButtonHandler(){
-  if(modalIsOpen){
-    document.body.style.overflow = "auto";
-    modalEl.classList.remove("modal--visible");
-  } else{
-    document.body.style.overflow = "hidden";
-    modalEl.classList.add("modal--visible");
-  }
-
-  modalIsOpen = !modalIsOpen;
 }
 
 modalEl.onclick = (event) => {
